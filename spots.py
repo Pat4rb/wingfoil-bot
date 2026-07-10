db03f806b84b346d84759c5c15080f208333b206
Pat4rb <patrick.viererbe@gmail.com>
2026-07-10 15:47:41 +0200

    Add SPOTS list with location data

---
+++ spots.py
@@ -0,0 +1,34 @@
+SPOTS = [
+
+    {
+        "name": "Walchensee",
+        "lat": 47.590,
+        "lon": 11.330,
+        "ideal_wind": ["N", "NE"],
+        "thermal_factor": 1.0
+    },
+
+    {
+        "name": "Alpsee Immenstadt",
+        "lat": 47.570,
+        "lon": 10.210,
+        "ideal_wind": ["W", "NW"],
+        "thermal_factor": 0.8
+    },
+
+    {
+        "name": "Rottachsee",
+        "lat": 47.650,
+        "lon": 10.280,
+        "ideal_wind": ["W", "NW"],
+        "thermal_factor": 0.9
+    },
+
+    {
+        "name": "Silvaplanersee",
+        "lat": 46.460,
+        "lon": 9.790,
+        "ideal_wind": ["SW"],
+        "thermal_factor": 1.2
+    }
+]
